ed84e3837b86b98a60892d74ad783845f9ebd688
Ethan Smith <ethansmith.dev@gmail.com>
2020-01-06 02:43:18 -0500

    I decided that I like the the result pointer in the function parameters.

--- src/vmath.c
+++ src/vmath.c
@@ -3,56 +3,47 @@
 
 /* 2 Dimensional Vector Implementation
  */
-float vec2_get_mag(const Vec2* v)
+float vec2_get_mag(Vec2* v)
 {
 	return sqrtf(pow(v->x, 2) + pow(v->y, 2));
 }
 
-float vec2_get_angle(const Vec2* v1, const Vec2* v2)
+float vec2_get_angle(Vec2* v1, Vec2* v2)
 {
 	return atanf(v1->y / v1->x) - atanf(v2->y / v2->x);
 }
 
-Vec2 vec2_add(const Vec2* v1, const Vec2* v2)
+Vec2* vec2_add(Vec2* v1, Vec2* v2, Vec2* result)
 {
-	Vec2 result;
-	result.x = v1->x + v2->x;
-	result.y = v1->y + v2->y;
+	result->x = v1->x + v2->x;
+	result->y = v1->y + v2->y;
 	return result;
 }
-Vec2 vec2_sub(const Vec2* v1, const Vec2* v2)
+Vec2* vec2_sub(Vec2* v1, Vec2* v2, Vec2* result)
 {
-	Vec2 result;
-	result.x = v1->x - v2->x;
-	result.y = v1->y - v2->y;
+	result->x = v1->x - v2->x;
+	result->y = v1->y - v2->y;
 	return result;
 }
-float vec2_dot(const Vec2* v1, const Vec2* v2)
+float vec2_dot(Vec2* v1, Vec2* v2)
 {
 	return vec2_get_mag(v1) * vec2_get_mag(v2) * cos(vec2_get_angle(v1, v2));
 }
 
-Vec2 vec2_scale(const Vec2* v1, float a)
+Vec2* vec2_scale(Vec2* v1, float a, Vec2* result)
 {
-	Vec2 result;
-	result.x = v1->x * a;
-	result.y = v1->y * a;
+	result->x = v1->x * a;
+	result->y = v1->y * a;
 	return result;
 }
 
-Vec2 vec2_rotate(const Vec2* v1, float a)
+Vec2* vec2_rotate(Vec2* v1, float a, Vec2* result)
 {
-	Vec2 result;
-	result.x = cos(a)*(v1->x) - sin(a)*(v1->y);
-	result.y = sin(a)*(v1->x) + cos(a)*(v1->y);
+	result->x = cos(a)*(v1->x) - sin(a)*(v1->y);
+	result->y = sin(a)*(v1->x) + cos(a)*(v1->y);
 	return result;
 }
 
 /* 3 Dimensional Vector Implementation
  */
-float vec3_get_mag(const Vec3* v)
-{
-	return sqrt(pow(v->x,2) + pow(v->y,2) + pow(v->z,2));
-}
-
-//TODO finish implementing the 3D stuff
+// TODO add Vec3 implementation;
