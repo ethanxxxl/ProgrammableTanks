e59cafba7c6da4b6db93efdc55e2a6a9a945c1cf
Ethan Smith <ethansmith.dev@gmail.com>
2020-01-12 02:38:53 -0500

    Made unchanged parameters consant, added a unit vector function

--- include/vmath.h
+++ include/vmath.h
@@ -20,29 +20,32 @@ typedef struct Vec4
 
 /* 2 Dimensional Vector Functions
  */
-float vec2_get_mag(Vec2* v);
-float vec2_get_angle(Vec2* v1, Vec2* v2);
+float vec2_get_mag(const Vec2* v);
+float vec2_get_angle(const Vec2* v1, const Vec2* v2);
 
 // basic operations
-Vec2* vec2_add(Vec2* v1, Vec2* v2, Vec2* result);
-Vec2* vec2_sub(Vec2* v1, Vec2* v2, Vec2* result);
+Vec2* vec2_add(const Vec2* v1, const Vec2* v2, Vec2* result);
+Vec2* vec2_sub(const Vec2* v1, const Vec2* v2, Vec2* result);
 // Vec3* vec2_cross(Vec2* v1, Vec2* v2, Vec2* result);
-float vec2_dot(Vec2* v1, Vec2* v2);
+float vec2_dot(const Vec2* v1, const Vec2* v2);
 
-Vec2* vec2_scale(Vec2* v1, float a, Vec2* result);
+Vec2* vec2_scale(const Vec2* v1, float a, Vec2* result);
 
-Vec2* vec2_rotate(Vec2* v1, float a, Vec2* result);
+Vec2* vec2_rotate(const Vec2* v1, float a, Vec2* result);
+
+// gets a unit vector from v1
+Vec2* vec2_get_unitv(const Vec2* v1, Vec2* result);
 
 
 /* 3 Dimensional Vector Functions
  */
-float vec3_get_mag(Vec2* v);
-float vec3_get_angle(Vec2* v1, Vec2* v2);
+float vec3_get_mag(const Vec2* v);
+float vec3_get_angle(const Vec2* v1, const Vec2* v2);
 
-Vec3* vec3_add(Vec2* v1, Vec2* v2, Vec2* result);
-Vec3* vec3_sub(Vec2* v1, Vec2* v2, Vec2* result);
-Vec4* vec3_cross(Vec2* v1, Vec2* v2, Vec2* result);
-float vec3_dot(Vec2* v1, Vec2* v2);
+Vec3* vec3_add(const Vec2* v1, const Vec2* v2, Vec2* result);
+Vec3* vec3_sub(const Vec2* v1, const Vec2* v2, Vec2* result);
+Vec4* vec3_cross(const Vec2* v1, const Vec2* v2, Vec2* result);
+float vec3_dot(const Vec2* v1, const Vec2* v2);
 
-Vec3* vec3_scale(Vec2* v1, float a, Vec2* result);
+Vec3* vec3_scale(const Vec2* v1, float a, Vec2* result);
 #endif
--- src/vmath.c
+++ src/vmath.c
@@ -3,45 +3,51 @@
 
 /* 2 Dimensional Vector Implementation
  */
-float vec2_get_mag(Vec2* v)
+float vec2_get_mag(const Vec2* v)
 {
 	return sqrtf(pow(v->x, 2) + pow(v->y, 2));
 }
 
-float vec2_get_angle(Vec2* v1, Vec2* v2)
+float vec2_get_angle(const Vec2* v1, const Vec2* v2)
 {
 	return atanf(v1->y / v1->x) - atanf(v2->y / v2->x);
 }
 
-Vec2* vec2_add(Vec2* v1, Vec2* v2, Vec2* result)
+Vec2* vec2_add(const Vec2* v1, const Vec2* v2, Vec2* result)
 {
 	result->x = v1->x + v2->x;
 	result->y = v1->y + v2->y;
 	return result;
 }
-Vec2* vec2_sub(Vec2* v1, Vec2* v2, Vec2* result)
+
+Vec2* vec2_sub(const Vec2* v1, const Vec2* v2, Vec2* result)
 {
 	result->x = v1->x - v2->x;
 	result->y = v1->y - v2->y;
 	return result;
 }
-float vec2_dot(Vec2* v1, Vec2* v2)
+float vec2_dot(const Vec2* v1, const Vec2* v2)
 {
 	return (v1->x * v2->x) + (v1->y * v2->y);
 }
 
-Vec2* vec2_scale(Vec2* v1, float a, Vec2* result)
+Vec2* vec2_scale(const Vec2* v1, float a, Vec2* result)
 {
 	result->x = v1->x * a;
 	result->y = v1->y * a;
 	return result;
 }
 
-Vec2* vec2_rotate(Vec2* v1, float a, Vec2* result)
+Vec2* vec2_rotate(const Vec2* v1, float a, Vec2* result)
 {
 	result->x = cos(a)*(v1->x) - sin(a)*(v1->y);
 	result->y = sin(a)*(v1->x) + cos(a)*(v1->y);
 	return result;
+}
+
+Vec2* vec2_get_unitv(const Vec2* v1, Vec2* result)
+{
+	return vec2_scale(v1, 1/vec2_get_mag(v1), result);
 }
 
 /* 3 Dimensional Vector Implementation
